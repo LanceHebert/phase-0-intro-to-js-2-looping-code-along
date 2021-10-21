f27c1d5e455102c2b9614fc1878dd3217d214e16
Lance Hebert <LSUHEBERT@gmail.com>
2021-10-20 19:38:26 -0700

    Looping code along completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+// 	for (let i = 0; i < gifts.length; i++) {
+// 		console.log(`Wrapped ${gifts[i]} and added a bow!`);
+// 		debugger;
+// 	}
+// 	return gifts;
+// }
+// wrapGifts(gifts);
+
+// const newArray = [];
+
+// function writeCards(arrayStringNames, eventName) {
+// 	for (i = 0; i < arrayStringNames.length; i++) {
+// 		newArray.push(
+// 			`Thank you, ${arrayStringNames[i]}, for the wonderful ${eventName} gift!`
+// 		);
+// 	}
+// 	return newArray;
+// }
+
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+// console.log(newArray);
+
+function countDown(number) {
+	while (number >= 0) {
+		if (number === 10) {
+			console.log(`T-minus ${number}`);
+			number--;
+		} else {
+			console.log(number);
+			number--;
+		}
+	}
+}
+countDown(10);
